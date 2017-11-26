fdd224e8a396fcdee13d834902b7845ac1f89fd1
kazuya-seki <track613armory@gmail.com>
2017-11-26 18:33:54 +0900

    一旦redux抜きでtweet一覧表示するように

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,105 @@
 /* @flow */
+import Axios from 'axios'
 import React from 'react'
 import styles from './styles.css'
 
-export default function Home () {
-  return <div className={styles.Home}>
-    <div className={styles.title}>Home</div>
-  </div>
+import { auth, twitterAuthProvider } from '../firebase';
+
+const addQueryParam = (url, params) => {
+  url += "?";
+  let param_strs = [];
+  for(let key in params){
+    param_strs.push( key + "=" + params[key] );
+  }
+  url += param_strs.join("&");
+  
+  return url;
+}
+
+const twitterQueryParamFactory = (key, secret, url) => {
+  return {
+    key,
+    secret,
+    url
+  }
+}
+
+const twitterAPIUrl = "https://us-central1-hello-firebase-26b50.cloudfunctions.net/execute";
+const urlRegex = new RegExp(/(https?:\/\/\S+)/g); 
+
+export default class Home extends React.Component {
+
+  constructor(){
+    super();
+    this.state = {
+      tweets: []
+    };
+  }
+
+  componentDidMount(){
+    let loadLinkedTweets = this.loadLinkedTweets.bind(this);
+    auth.onAuthStateChanged(function(user) {
+      if (user) {
+        loadLinkedTweets();
+      } else {
+        auth.signInWithPopup(twitterAuthProvider).then(function(result) {
+          var token = result.credential.accessToken;
+          var secret = result.credential.secret;
+
+          localStorage.setItem("twitter:tokens", JSON.stringify({
+            token,
+            secret
+          }))
+      
+          loadLinkedTweets();
+      
+        }).catch(function(error) {
+          console.log(error);
+        });
+      }
+    });
+  }
+
+  loadLinkedTweets() {
+    let tokens = JSON.parse(localStorage.getItem("twitter:tokens"));
+    let self = this;
+    Axios.all([
+      Axios.get(addQueryParam(twitterAPIUrl, twitterQueryParamFactory(tokens.token, tokens.secret, 'favorites/list'))),
+      Axios.get(addQueryParam(twitterAPIUrl, twitterQueryParamFactory(tokens.token, tokens.secret, 'statuses/user_timeline')))
+    ]).then(function (response) {
+      let newTweets = response[0].data.concat(response[1].data).filter((tweet) => {return tweet.text.match(urlRegex)});
+      self.setState({ tweets: newTweets });
+    })
+  }
+
+  signIn() {
+    auth.signInWithPopup(twitterAuthProvider).then(function(result) {
+      var token = result.credential.accessToken;
+      var secret = result.credential.secret;
+
+      localStorage.setItem("twitter:tokens", JSON.stringify({
+        token,
+        secret
+      }))  
+    }).catch(function(error) {
+      console.log(error);
+    });
+  }
+
+  render (){
+    return <div className={styles.Home}>
+      <button onClick={this.loadLinkedTweets.bind(this)}>reload</button>
+      <ul>  
+        {this.state.tweets.map((tweet) => {
+          let linkUrl = tweet.text.match(urlRegex)[0];
+
+          return (<li>
+              <p>{tweet.text}</p>
+              <a href={linkUrl} target="_blank">{linkUrl}</a>
+            </li>
+          )    
+        })}
+      </ul>
+    </div>
+  }
 }
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,13 +2,17 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 
+import styles from './styles.css'
+
 export default function Layout (props: {children: any}) {
-  return <div className="Layout">
-    <ul>
+  return <div className={styles.Layout}>
+    <ul  className={styles.navigation}>
       <li><NavLink to="/">Home</NavLink></li>
-      <li><NavLink to="/about">About</NavLink></li>
     </ul>
     <hr/>
-    {props.children}
+
+    <div  className={styles.main}>
+      {props.children}
+    </div>
   </div>
 }
