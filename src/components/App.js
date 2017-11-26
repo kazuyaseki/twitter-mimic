f4cdf0335cdbd2cdf56943cd37d64b00fe5aa75c
kazuya-seki <track613armory@gmail.com>
2017-11-26 18:32:31 +0900

    いらんかったのでabout.js削除

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,6 @@ import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom'
 import { history } from '../store'
 import HomeContainer from '../containers/HomeContainer'
 import Layout from './Layout'
-import About from './About'
 import NotFound from './NotFound'
 
 export default function App (
@@ -14,7 +13,6 @@ export default function App (
     <BrowserRouter history={history}>
       <Layout children={
         <Switch>
-          <Route path="/about" component={About}/>
           <Route path="/404" exact component={NotFound}/>
           <Route path="/" exact component={HomeContainer}/>
           <Route path="*" component={() => <Redirect to="/404"/>}/>
