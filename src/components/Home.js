5dce8e8ec7d6802c52b040b959d6b1590cb42466
kazuya-seki <track613armory@gmail.com>
2017-11-27 20:06:46 +0900

    tweetを日付で降順にソートするように変更

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,6 +2,7 @@
 import Axios from 'axios'
 import React from 'react'
 import styles from './styles.css'
+import moment from 'moment';
 
 import { auth, twitterAuthProvider } from '../firebase';
 
@@ -60,7 +61,19 @@ export default class Home extends React.Component {
       Axios.get(addQueryParam(twitterAPIUrl, twitterQueryParamFactory(tokens.token, tokens.secret, 'favorites/list'))),
       Axios.get(addQueryParam(twitterAPIUrl, twitterQueryParamFactory(tokens.token, tokens.secret, 'statuses/user_timeline')))
     ]).then(function (response) {
-      let newTweets = response[0].data.concat(response[1].data).filter((tweet) => {return tweet.text.match(urlRegex)});
+      let newTweets = response[0].data.concat(response[1].data)
+        .filter((tweet) => {return tweet.text.match(urlRegex)})
+        .sort((a, b) => {
+          let aDate = new Date(a.created_at);
+          let bDate = new Date(b.created_at);
+
+          if(aDate.getTime() > bDate.getTime())
+            return -1;
+          else if(bDate.getTime() > aDate.getTime())
+            return 1;
+          else
+            return 0;
+        });
       self.props.updateTweets(newTweets);
     })
   }
@@ -85,9 +98,11 @@ export default class Home extends React.Component {
       <ul>  
         {this.props.tweets.map((tweet, index) => {
           let linkUrl = tweet.text.match(urlRegex)[0];
+          let date = moment(tweet.created_at);
 
           return (<li key={index}>
               <p>{tweet.text}</p>
+              <p>{date.format("YYYY年 MM月DD日")}</p>
               <a href={linkUrl} target="_blank">{linkUrl}</a>
             </li>
           )    
